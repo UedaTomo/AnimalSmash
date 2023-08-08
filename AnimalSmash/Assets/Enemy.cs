06f8697a6c34a26fbde458628668121786966d67
shibatosora <g2227043@st.t-kougei.ac.jp>
2023-08-08 22:49:51 +0900

    敵のサイズ縮小とデスポーン

--- AnimalSmash/Assets/Enemy.cs
+++ AnimalSmash/Assets/Enemy.cs
@@ -5,16 +5,35 @@ using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
-    public Transform traget;
+    public Transform target;        // ターゲット
+    public float shrinkRate = 1.0f; // 縮小率
+
     private NavMeshAgent myAgent;
-    // Start is called before the first frame update
-    void Start()
+    private Vector3 initialScale;
+    private float initialDistance;
+
+    private void Start()
     {
         myAgent = GetComponent<NavMeshAgent>();
+        initialScale = transform.localScale;
+        initialDistance = Vector3.Distance(transform.position, target.position);
     }
-    // Update is called once per frame
-    void Update()
+
+    private void Update()
     {
-        myAgent.SetDestination(traget.position);
+        myAgent.SetDestination(target.position);
+        // ターゲットに移動
+        float distanceToTarget = Vector3.Distance(transform.position, target.position);
+        // オブジェクトとターゲットの距離を取得
+        float scale = Mathf.Lerp(1.0f, initialScale.x * shrinkRate, distanceToTarget / initialDistance);
+        // 距離が近づくにつれてオブジェクトを縮小
+        transform.localScale = new Vector3(scale, scale, scale);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("destroy"))
+        {
+            Destroy(gameObject);
+        }
     }
 }
