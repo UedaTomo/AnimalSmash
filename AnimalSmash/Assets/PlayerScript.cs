850fa4be64fe12a0dfe1b1079dca5d3176f71a41
UedaTomo <g2227010@st.t-kougei.ac.jp>
2023-09-26 18:00:14 +0900

    コントローラー操作とナビメッシュ修正しました

--- AnimalSmash/Assets/PlayerScript.cs
+++ AnimalSmash/Assets/PlayerScript.cs
@@ -15,25 +15,25 @@ public class PlayerScript : MonoBehaviour
     void Update()
     {
         // Wキー（前方移動）
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.W) || Input.GetAxis("Vertical") > 0)
         {
             transform.position += speed * transform.forward * Time.deltaTime;
         }
 
         // Sキー（後方移動）
-        if (Input.GetKey(KeyCode.S))
+        if (Input.GetKey(KeyCode.S) || Input.GetAxis("Vertical") < 0)
         {
             transform.position -= speed * transform.forward * Time.deltaTime;
         }
 
         // Dキー（右移動）
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.D) || Input.GetAxis("Horizontal") > 0)
         {
             transform.position += speed * transform.right * Time.deltaTime;
         }
 
         // Aキー（左移動）
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A) || Input.GetAxis("Horizontal") < 0)
         {
             transform.position -= speed * transform.right * Time.deltaTime;
         }
