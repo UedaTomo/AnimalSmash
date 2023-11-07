11847458aa8181a9dedb687271d7eece1fc809ae
shion-018 <noname09514@yahoo.co.jp>
2023-11-07 15:56:37 +0900

    title&result

---
+++ AnimalSmash/Assets/HelthScript.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+using UnityEngine.SceneManagement;
+
+public class HelthScript : MonoBehaviour
+{
+    public int playerCount; // écã@êî
+    public TextMeshProUGUI HP;
+    public TextMeshProUGUI GameOver;
+    public TextMeshProUGUI GameOverComment;
+    public TextMeshProUGUI WaitKey;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerCount = 5;
+        HP.text = "Å~ " + playerCount;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void IsGameover()
+    {
+        GameOver.text = "Game Over";
+        GameOverComment.text = "you couldn't protect the village ...";
+        WaitKey.text = "<Push B to Title>";
+
+        if (Input.GetKey("joystick button 1") || Input.GetKey("joystick button 2") || Input.GetKey("joystick button 3") || Input.GetKey("joystick button 4"))
+        {
+            SceneManager.LoadScene("title");
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("enemy"))
+        {
+            Destroy(other.gameObject);
+            if (playerCount > 0)
+            {
+                    
+                    playerCount -= 1;
+                    HP.text = "Å~ " + playerCount;
+
+            }
+        }
+        
+
+        if (playerCount <= 0)
+        {
+            IsGameover();
+        }
+    }
+}
