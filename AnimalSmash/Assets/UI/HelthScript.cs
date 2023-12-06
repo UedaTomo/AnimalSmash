12994b2c9b3ecb111c53bbfd170be330cbf8ea16
UedaTomo <g2227010@st.t-kougei.ac.jp>
2023-12-06 15:23:17 +0900

    一旦ね

--- AnimalSmash/Assets/UI/HelthScript.cs
+++ AnimalSmash/Assets/UI/HelthScript.cs
@@ -44,10 +44,10 @@ public class HelthScript : MonoBehaviour
             playerCount = nowHP;
             //VictoryImage.color = newColor;
 
-            if (Input.GetKey("joystick button 1")||Input.GetKey("joystick button 2")||Input.GetKey("joystick button 3")||Input.GetKey("joystick button 4")||(Input.GetKeyDown(KeyCode.Space)))
-            {
-                SceneManager.LoadScene("title");
-            }
+            //if (Input.GetKey("joystick button 1")||Input.GetKey("joystick button 2")||Input.GetKey("joystick button 3")||Input.GetKey("joystick button 4")||(Input.GetKeyDown(KeyCode.Space)))
+            //{
+                //SceneManager.LoadScene("title");
+            //}
             //VictoryImage.GetComponent<SpriteRenderer>().color = new Color(0, 0, 0, 255);
         }
         //if(playerCount > 0)
