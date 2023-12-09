3da5e27e36a1ed031be1de7c2170a054087a06ee
shibatosora <114388649+shibatosora@users.noreply.github.com>
2023-12-09 14:03:37 +0900

    ボスモーション等

--- AnimalSmash/Assets/PlayerAction/damage.cs
+++ AnimalSmash/Assets/PlayerAction/damage.cs
@@ -2,42 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
+using UnityEngine.UI;
+using TMPro;
 
 public class damage : MonoBehaviour
 {
     private int _damageLevel = 1;
-    private int _damage = 1;
+    public int _damage = 1; 
+    private int _conbo = 0; //ÉRÉìÉ{êî
     private Rigidbody rb;
-    public GameObject smash;
+    [SerializeField] private GameObject smash;
+    [SerializeField] private GameObject _bossHit;
+    [SerializeField] private AudioSource _source;
+    [SerializeField] private AudioClip koyabreak; //åöï®Ç™è≠ÇµïˆÇÍÇÈ
+    [SerializeField] private AudioClip enemydie;
+    public float rotationSpeed = 5.0f; // âÒì]ÇÃë¨Ç≥
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+
+        rb.angularVelocity = new Vector3(-rotationSpeed, 0, 0); // í«â¡
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(new Vector3(-20, 0, 0) * Time.deltaTime);
-    }
-    public void BasicDamage(bool enemy)
-    {
-        if (enemy == true)
-            _damage = 40;
-        if (enemy == false)
-            _damage = 1;
+        transform.Rotate(Vector3.right, rotationSpeed * Time.deltaTime);
     }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Boss"))
         {
-            other.GetComponent<BossScript>().HP(_damage, _damageLevel);
+            Vector3 effectPosition = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z-5);
+            Instantiate(_bossHit, effectPosition, Quaternion.identity);
+            _source.PlayOneShot(koyabreak); //çƒê∂
+            other.GetComponent<BossScript>().HP(_conbo,_damage, _damageLevel);
             Destroy(this.gameObject);
         }
         if (other.CompareTag("enemy"))
         {
+            _conbo++;
             _damageLevel *= 2;
             Instantiate(smash, this.transform.position, Quaternion.identity);
+            _source.PlayOneShot(enemydie); //çƒê∂
             Destroy(other.gameObject);
         }
 
